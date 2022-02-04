5335c39f3e3ac0bb3b919b9427103fbde98bc13a
Andy McAliley <wmcaliley@usgs.gov>
2022-02-03 17:04:48 -0700

    Write assemble_data (untested)

--- 2_process/src/process_mntoha.py
+++ 2_process/src/process_mntoha.py
@@ -1,7 +1,39 @@
 import os
 import zipfile
+import requests
+import urllib
+
 import numpy as np
 import pandas as pd
+from scipy import interpolate
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+def elevation_function(lat, lon):
+    """
+    Query service using lat, lon. add the elevation values as a new column.
+    Thanks to https://gis.stackexchange.com/questions/338392/getting-elevation-for-multiple-lat-long-coordinates-in-python
+
+    :param lat: latitude values in decimal degrees
+    :param lon: longitude values
+    :returns: elevation at the specified coordinates
+
+    """
+    # USGS Elevation Point Query Service
+    url = r'https://nationalmap.gov/epqs/pqs.php?'
+    # define rest query params
+    params = {
+        'output': 'json',
+        'x': lon,
+        'y': lat,
+        'units': 'Meters'
+    }
+
+    # format query string and return query value
+    result = requests.get((url + urllib.parse.urlencode(params)))
+    elevation = result.json()['USGS_Elevation_Point_Query_Service']['Elevation_Query']['Elevation']
+
+    return elevation
 
 def unzip_files(files, source_dir, destination_dir):
     """
@@ -29,15 +61,118 @@ def unzip_files(files, source_dir, destination_dir):
     return destination_files
 
 
-def assemble_data(lake_metadata):
+def assemble_data(lake_metadata, unzipped_dir, config):
     """
-    TODO: Docstring for assemble_data.
+    Assemble raw data into equal-length sequences for training/testing
 
     :param lake_metadata: TODO
     :returns: TODO
 
     """
-    pass
+    # Read hyperparameters
+    sequence_length = config['sequence_length']
+    sequence_offset = config['sequence_offset']
+    # pad time before 1st obs, e.g., 100 days
+    spinup_time = config['spinup_time'] 
+
+    # Read temperature observations
+    obs = pd.read_csv(config['obs_file'], parse_dates=['date'])
+    # lake depths to use for LSTM
+    depths = np.array(config['depths'])
+    # Round depth to nearest neighbor
+    depth_interpolant = interpolate.interp1d(depths, depths,
+                                             kind='nearest',
+                                             assume_sorted=False)
+    obs['interpolated_depth'] = depth_interpolant(obs.depth)
+
+    # Add elevation to metadata
+    lake_metadata['elevation'] = lake_metadata.apply(
+        lambda row: elevation_function(
+            row['centroid_lat'], row['centroid_lon']),
+        axis=1)
+
+    for lake in lake_metadata:
+        # lake observations
+        lake_obs = obs[obs.site_id==lake.site_id].copy()
+
+        ### Fill NaNs for missing obs
+        # uses spinup_time, lake_obs, depths, 
+        # Now, let's try a naive way: create an empty dataframe and merge on that.
+        spinup_time_td = np.timedelta64(spinup_time, 'D')
+        # buffer start_date by spinup_time days
+        start_date = lake_obs.date.min() - spinup_time_td
+        end_date = lake_obs.date.max()
+        date_range = pd.date_range(start=start_date, end=end_date)
+
+        midx = pd.MultiIndex.from_product([date_range, depths], names=['date', 'interpolated_depth'])
+        # Average duplicate measurements (same day, same nearest depth)
+        lake_obs_midx = lake_obs.groupby(['date', 'interpolated_depth']).temp.mean().to_frame()
+
+        df_timesteps = lake_obs_midx.reindex(midx, fill_value=np.NaN)
+        # reshape to have one column per depth
+        df_timesteps = df_timesteps.unstack()
+        # flatten column levels
+        df_timesteps.columns = [' '.join([str(s) for s in col]).strip() for col in df_timesteps.columns.values]
+        # Now df_timesteps has dimensions of len(date_range) by len(depths)
+        # values are NaN wherever there are no observations
+
+        ### Join drivers
+
+        #2_process/tmp/drivers_mntoha/inputs_01_N48.00-49.50_W89.50-97.25/nldas_meteo_N48.0625-48.0625_W90.6875-90.6875.csv
+        drivers_file = f'{destination_dir}/drivers_mntoha/inputs_{lake.group_id}/{lake.meteo_filename}'
+        drivers = pd.read_csv(drivers_file, parse_dates=['time'], index_col='time')
+        # TODO: Check for nans
+        # drivers.isna().any()
+        df_timesteps = df_timesteps.join(drivers, how='left')
+
+        ### Join clarity
+        clarity_file = f'{destination_dir}/drivers_mntoha/clarity_{lake.group_id}/gam_{lake.site_id}_clarity.csv'
+        clarity = pd.read_csv(clarity_file, parse_dates=['date'], index_col='date')
+        df_timesteps = df_timesteps.join(clarity, how='left')
+
+        ### Join ice flags
+        ice_flags_file = f'{destination_dir}/drivers_mntoha/ice_flags_{lake.group_id}/pb0_{lake.site_id}_ice_flags.csv'
+        ice_flags = pd.read_csv(ice_flags_file, parse_dates=['date'], index_col='date')
+        df_timesteps = df_timesteps.join(ice_flags, how='left')
+
+        ## Get attributes
+        df_timesteps['area'] = lake['area']
+        df_timesteps['lon'] = lake['centroid_lon']
+        df_timesteps['lat'] = lake['centroid_lat']
+        df_timesteps['elevation'] = lake['elevation']
+        # Now df_timesteps is one long timeseries with depth-specific observations, drivers, and attributes as columns
+
+        ## Split into segments
+
+        # Let's make some rules:
+        # 1. 400 day sequences
+        # 2. First 100 days are spinup, so don't have observations then
+        # 3. Each next sequence starts 200 days later than the previous
+        # 4. Discard sequences that lack observations
+        # 5. We know going in that the first and last sequence have observations
+
+        # Convert dataframe to numpy array
+        arr_timesteps = df_timesteps.to_numpy(dtype=np.float32)
+        # Create a strided view into arr_timesteps
+        # This creates a numpy object with sequences of length sequence_length
+        # and offsets of sequence_offset without making a copy the array
+        all_sequences = np.lib.stride_tricks.sliding_window_view(arr_timesteps, sequence_length, axis=0)[::sequence_offset, :]
+        # shape is now (# sequences, # features + depths, sequence_length)
+        # The first `len(depths)` elements in the middle dimension are observations
+
+        # Mask sequences that have only NaN observations
+        mask_nan_sequences = np.any(np.any(~np.isnan(all_sequences[:, :len(depths), :]), axis=2), axis=1)
+        # sequences to keep
+        lake_sequences = all_sequences[mask_nan_sequences, :, :].transpose((0, 2, 1))
+
+        # also keep the last `sequence_length` days, since the final observation is always non-NaN,
+        # unless all_sequences already has that last sequence 
+        if (arr_timesteps.shape[0] - sequence_length) % sequence_offset != 0:
+            lake_sequences = np.concatenate((lake_sequences, np.array([arr_timesteps[-sequence_length:, :]])), axis=0)
+
+        sequences_file = os.path.join('2_process', 'out', f'sequences_{lake.site_id}.npy')
+        np.save(sequences_file, lake_sequences, allow_pickle=True)
+
 
 def main(in_files, out_file, config):
     """
