6c6defe9ef5f2c8b8d183758167f62a8d9cb7934
Andy McAliley <wmcaliley@usgs.gov>
2022-04-12 10:24:27 -0600

    Clean up formatting

--- 3_train/src/train.py
+++ 3_train/src/train.py
@@ -1,10 +1,10 @@
 import os
+import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 from torch.optim import Adam
 
-import numpy as np
 import sys
 sys.path.insert(1, '3_model/src')
 from models import Model
@@ -14,32 +14,32 @@ class SequenceDataset(Dataset):
     """
     Custom Dataset class for sequences that include dynamic and static inputs,
     and multiple outputs
-
+    
     __len__ returns the number of sequences
     
     __getitem__ returns dynamic input features, static input features, and
     temperature observations at all depths for one sequence
+    
+    :Example:
 
-    Example:
 
     >>> npz = np.load("2_process/out/mntoha/train.npz")
+    >>> n_depths = len(npz['depths'])
     >>> n_dynamic = len(npz['dynamic_features_all'])
     >>> n_static = len(npz['static_features_all'])
-    >>> n_depths = len(npz['depths'])
     >>> data = torch.from_numpy(npz['data'])
-    >>> dataset = SequenceDataset(data, n_depths, n_dynamic, n_static)                            
+    >>> dataset = SequenceDataset(data, n_depths, n_dynamic, n_static)
     >>> # number of sequences
     >>> len(dataset)
     19069
-    >>> # dynamic features, static features, and temperatures at depths 
+    >>> # dynamic features, static features, and temperatures at depths
     >>> # for first sequence
     >>> for e in dataset[0]:
     >>>     print(e.shape)
-    >>> 
+    >>>
     torch.Size([400, 9])
     torch.Size([400, 4])
     torch.Size([400, 49])
-
     """
     def __init__(self, sequences, n_outputs, n_dynamic, n_static):
         """
@@ -50,7 +50,7 @@ class SequenceDataset(Dataset):
 
         """
         # The number of sequences is the size of the first dimension
-        self.len = sequences.shape[0]
+        self.len = len(sequences)
         self.n_outputs = n_outputs
         self.n_dynamic = n_dynamic
         self.n_static = n_static
