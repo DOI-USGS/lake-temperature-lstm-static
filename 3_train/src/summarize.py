712a9cc9d7efe57c3d69e4f5d381961f59f6f43e
Andy McAliley <wmcaliley@usgs.gov>
2022-04-21 09:57:40 -0600

    Add docstrings
    Improve some variable names

--- 3_train/src/summarize.py
+++ 3_train/src/summarize.py
@@ -4,23 +4,46 @@ import numpy as np
 import pandas as pd
 
 # Don't write these out to the summary file (they're long lists)
-files_to_exclude = [
+FILES_TO_EXCLUDE = [
     'train_data_means',
     'train_data_stds',
     'train_losses',
     'valid_losses'
 ]
 
-def read_npz_as_dataframe(filename):
-    npz = np.load(filename)
+def read_npz_as_dataframe(npz_name):
+    """
+    Read a numpy npz and convert to a pandas DataFrame.
+
+    Exclude specific files that are inside the npz from the DataFrame because
+    they are long lists that don't need to be saved to a summary file.
+
+    :param npz_name: Name of npz to read
+    :returns: Pandas DataFrame containing contents of npz, with the names of
+        the files in the npz as column names
+
+    """
+    npz = np.load(npz_name)
     # Make every item in the dict a one-element list so that pandas can read it in easily
-    metadata_dict = {file: [npz[file]] for file in npz.files if not file in files_to_exclude}
+    metadata_dict = {file: [npz[file]] for file in npz.files if not file in FILES_TO_EXCLUDE}
     df = pd.DataFrame.from_dict(metadata_dict, orient='columns')
     return df
 
 
 def main(summary_filepath):
+    """
+    Summarize all trained models in all subdirectories of a given root directory.
 
+    Read in all .npz files in the subdirectories of the root and assume that
+    all .npz files are metadata files saved to accompany trained models.
+    Extract information to summarize from those .npz files, and save it to a
+    csv. Use the combination of the run_id and the model_id to create a unique
+    identifier for every trained model, and save that in the first column of
+    the summary csv. 
+
+    :param summary_filepath: File path of the summarizing csv to create
+
+    """
     metadata_file_pattern = '*.npz'
     # Search the directory that will contain the summary file 
     # for all metadata npz files, recursively
