f87e40262e4b74684d98e3fbe5b1ac17b0219978
Andy McAliley <AndyMcAliley@users.noreply.github.com>
2022-06-09 08:44:21 -0600

    Format docstring for rds_to_csv
    Co-authored-by: hcorson-dosch <54007288+hcorson-dosch@users.noreply.github.com>

--- 2_process/src/convert_rds_to_csv.R
+++ 2_process/src/convert_rds_to_csv.R
@@ -1,4 +1,6 @@
-# Convert an rds file that contains a single dataframe to csv
+#' @description Convert an rds file that contains a single dataframe to csv
+#' @param rds_file Path to rds file to convert
+#' @param csv_file Path to csv to save
 rds_to_csv <- function(rds_file, csv_file) {
   df <- readRDS(rds_file)
   write.csv(df, csv_file, row.names=FALSE)
