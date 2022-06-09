1d34232f5834ba1aeb4ca273d243add22fd2d7ae
Andy McAliley <AndyMcAliley@users.noreply.github.com>
2022-06-09 08:51:05 -0600

    Create directory if needed in rds_to_csv
    Co-authored-by: hcorson-dosch <54007288+hcorson-dosch@users.noreply.github.com>

--- 2_process/src/convert_rds_to_csv.R
+++ 2_process/src/convert_rds_to_csv.R
@@ -2,6 +2,10 @@
 #' @param rds_file Path to rds file to convert
 #' @param csv_file Path to csv to save
 rds_to_csv <- function(rds_file, csv_file) {
+  destination_dir <- dirname(csv_file)
+  if (!dir.exists(destination_dir)) { 
+    dir.create(destination_dir, recursive=TRUE) 
+  }
   df <- readRDS(rds_file)
   write.csv(df, csv_file, row.names=FALSE)
 }
