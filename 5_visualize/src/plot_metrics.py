7cb1050396628c1e7e7514a5a2e830b86d97651e
Andy McAliley <wmcaliley@usgs.gov>
2022-09-08 17:22:23 -0600

    Add docstrings

--- 5_visualize/src/plot_metrics.py
+++ 5_visualize/src/plot_metrics.py
@@ -12,6 +12,13 @@ def load_predictions(predictions_filepath, doy_bin_width):
     Load the predictions for the training, validation, or testing set. Remove
     any NaN predictions. Compute residuals (as observed - predicted) and save
     them as an extra column in the dataframe.
+
+    :param predictions_filepath: Path to predictions csv
+    :param doy_bin_width: Number of days to bin day of year to. For example, a
+        doy_bin_width of 7 means that the values in the column doy_bin will
+        increase by 7 at a time. Useful for smoothing out plots of metrics by
+        day of year.
+
     """
     raw_preds = pd.read_csv(predictions_filepath, parse_dates=['date'])
     # remove nans
@@ -27,13 +34,25 @@ def load_predictions(predictions_filepath, doy_bin_width):
 
 def rms(df):
     """
-    Get RMS for a series
-    Useful as argument to groupby().aggregate()
+    Return root mean squared of a Pandas Series or Dataframe. Useful as
+    argument to groupby().aggregate().
+
+    :param df: Pandas Series or Dataframe to compute RMS for
+
     """
     return np.sqrt((df**2).mean())
 
 
 def plot_by_lake(agg_metric, lake_metadata_filepath, metric):
+    """
+    Create a scatter plot of modeled lakes with color indicating RMS error.
+
+    :param agg_metric: Pandas Dataframe with aggregated metrics for each lake
+    :param lake_metadata_filepath: Path to lake metadata csv
+    :param metric: Metric to plot, 'rmse' or 'bias'. Used to determine colormap
+    for points (diverging for bias, sequential for rmse)
+
+    """
     # Augment with lake metadata
     lake_metadata = pd.read_csv(lake_metadata_filepath, index_col=0)
     df_plot = pd.merge(agg_metric, lake_metadata, how='left', on='site_id')
@@ -62,7 +81,35 @@ def plot_metric(plot_filepath, predictions_filepath, metric, plot_by,
                 include_train_mean=False,
                 doy_bin_width=1):
     """
-    Plot a metric (e.g. RMS error) as a function of some variable (e.g. lake depth)
+    Plot a metric (e.g. RMS error) as a function of some variable (e.g. lake depth).
+    
+    This function allows plotting RMSE or bias by lake, by depth in the lake, or by day of year.
+    
+    There is also the option to include metrics for the mean of the training
+    set. This allows performance to be compared against the variance of the
+    observations, which is useful. For instance, RMSE is lower in winter, but
+    that lower RMSE comes about because lake temperatures vary less during
+    winter months, not by any virtue of the model. This is clear when the mean
+    of the training data is included in the plots. Note that the RMSE of the
+    mean of the training data is the standard deviation of the training data.
+
+    :param plot_filepath: Path to save plot image to. Image format is
+        determined from extension.
+    :param predictions_filepath: Path to predictions csv
+    :param metric: Metric to plot: 'rmse' or 'bias'
+    :param plot_by: Independent variable for evaluating metric: 'lake',
+        'depth', or 'doy'
+    :param lake_metadata_filepath: Path to lake metadata csv (Default value =
+        None). Only needed for plotting by lake, to get lake latitude and
+        longitude.
+    :param train_predictions_filepath: Path to training set predictions csv (Default value = None)
+    :param include_train_mean: Whether to include metrics for the mean of the
+        training set in the plot (Default value = False).
+    :param doy_bin_width: Number of days to bin day of year to (Default value =
+        1). For example, a doy_bin_width of 7 means that the values in the
+        column doy_bin will increase by 7 at a time. Useful for smoothing out
+        plots of metrics by day of year.
+
     """
     # Dictionary relating inputs to column names
     prediction_columns = {
