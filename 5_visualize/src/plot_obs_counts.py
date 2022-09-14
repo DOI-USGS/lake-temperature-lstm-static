899293d11f9027d7e971410d5f2bf2f35526f93a
Andy McAliley <wmcaliley@usgs.gov>
2022-09-14 11:22:10 -0600

    Add `ax` as argument to plot_obs_count

--- 5_visualize/src/plot_obs_counts.py
+++ 5_visualize/src/plot_obs_counts.py
@@ -7,11 +7,12 @@ import sys
 sys.path.insert(1, '5_visualize/src')
 from plot_metrics import load_predictions
 
-def plot_obs_count(predictions_filepath, lake_metadata_filepath, plot_by, **kwargs):
+def plot_obs_count(ax, predictions_filepath, lake_metadata_filepath, plot_by, **kwargs):
     """
     Plot the density of observation data as a histogram or hexbin plot, as a
     function of some variable (e.g. lake depth).
 
+    :param ax: matplotlib axes object to plot to
     :param predictions_filepath: Path to predictions csv to load
     :param lake_metadata_filepath: Path to lake metadata csv
     :param plot_by: Independent variable for plot: 
@@ -20,32 +21,30 @@ def plot_obs_count(predictions_filepath, lake_metadata_filepath, plot_by, **kwar
         the vertical axis.
     :param **kwargs: Keyword arguments to pass through to call to library
         plotting function (sns.histplot or plt.hexbin)
-    :returns: Matplotlib figure
 
     """
 
     preds = load_predictions(predictions_filepath, lake_metadata_filepath=lake_metadata_filepath)
 
-    fig, ax = plt.subplots()
     if plot_by == 'area':
         # Number of observations by lake surface area
         min_value = preds['area'].min()
         max_value = preds['area'].max()
         logbins = np.logspace(np.log10(min_value), np.log10(max_value), 50)
-        hist = sns.histplot(preds['area'], bins=logbins, ax=ax, **kwargs)
-        plt.xscale('log')
+        hist = sns.histplot(preds['area'], bins=logbins, ax=ax, edgecolor='black', linewidth=0.5, **kwargs)
+        ax.set_xscale('log')
         ax.set_xlabel('Lake surface area')
         ax.set_ylabel('Number of observations')
     elif plot_by == 'depth':
         # Number of observations by depth
         hist = sns.histplot(preds['interpolated_depth'], bins=50, ax=ax, **kwargs)
-        # plt.yscale('log')
+        # ax.set_yscale('log')
         ax.set_xlabel('Lake depth')
         ax.set_ylabel('Number of observations')
     elif plot_by == 'elevation':
         # Number of observations by elevation
         hist = sns.histplot(preds['elevation'], bins=50, ax=ax, **kwargs)
-        # plt.yscale('log')
+        # ax.set_yscale('log')
         ax.set_xlabel('Elevation')
         ax.set_ylabel('Number of observations')
     elif plot_by == 'doy':
@@ -63,7 +62,6 @@ def plot_obs_count(predictions_filepath, lake_metadata_filepath, plot_by, **kwar
         ax.invert_yaxis()
     else:
         raise ValueError(f'Plotting observation counts by {plot_by} is not supported')
-    return fig
 
 def save_obs_count_plot(plot_filepath, predictions_filepath, lake_metadata_filepath, plot_by, **kwargs):
     """
@@ -80,7 +78,8 @@ def save_obs_count_plot(plot_filepath, predictions_filepath, lake_metadata_filep
         plotting function (sns.histplot or plt.hexbin)
 
     """
-    fig = plot_obs_count(predictions_filepath, lake_metadata_filepath, plot_by, **kwargs)
+    fig, ax = plt.subplots()
+    plot_obs_count(ax, predictions_filepath, lake_metadata_filepath, plot_by, **kwargs)
     plt.tight_layout()
     fig.savefig(plot_filepath)
 
