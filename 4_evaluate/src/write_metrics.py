0616db43ef2cbdd6d4b53083be7afbf8d4df4326
Andy McAliley <wmcaliley@usgs.gov>
2022-09-17 16:25:26 -0600

    Write metrics to csv

--- 4_evaluate/src/write_metrics.py
+++ 4_evaluate/src/write_metrics.py
@@ -1,8 +1,33 @@
+import pandas as pd
 
-def write_metrics(metrics_filepath, predictions_filepath):
+import sys
+sys.path.insert(1, '4_evaluate/src')
+sys.path.insert(1, '5_visualize/src')
+from plot_metrics import load_predictions, rms
+
+
+def compute_metrics(predictions_filepath):
+    """
+    Compute metrics for a dataset
+    """
     preds = load_predictions(predictions_filepath)
     rmse_lake = preds.groupby('site_id')['residual'].aggregate(rms).rename('LSTM_rmse')
-    print(rms(preds['residual']), rmse_lake.median(), rmse_lake.mean())
+
+    results_dict = {
+        'RMSE': rms(preds['residual']),
+        'Median lake RMSE': rmse_lake.median(),
+        'Mean lake RMSE': rmse_lake.mean(),
+    }
+    return pd.DataFrame(results_dict)
+
+
+def write_metrics(metrics_filepath, predictions_filepath):
+    """
+    Write metrics to file
+    """
+    metrics = compute_metrics(predictions_filepath)
+    # print(rms(preds['residual']), rmse_lake.median(), rmse_lake.mean())
+    metrics.to_csv(metrics_filepath)
 
 
 if __name__ == '__main__':
