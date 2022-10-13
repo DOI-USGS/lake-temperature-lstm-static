84d036a45aff87df7c36569c56ea4c3bcce1744f
Andy McAliley <wmcaliley@usgs.gov>
2022-10-13 11:22:32 -0600

    Add docstrings

--- 4_evaluate/src/write_metrics.py
+++ 4_evaluate/src/write_metrics.py
@@ -5,10 +5,19 @@ sys.path.insert(1, '4_evaluate/src')
 sys.path.insert(1, '5_visualize/src')
 from plot_metrics import load_predictions, rms
 
+"""
+Write evaluation metrics for one model to a csv file. This will be a single row in a csv.
+"""
+
 
 def compute_metrics(predictions_filepath):
     """
-    Compute metrics for a dataset
+    Compute metrics for a dataset, including overall RMSE, median and mean RMSE
+    by lake, the total number of observations, and the number of lakes.
+
+    :param predictions_filepath: Path to csv with model predictions
+    :returns: DataFrame of metrics computed for the predictions
+
     """
     preds = load_predictions(predictions_filepath)
     rmse_lake = preds.groupby('site_id')['residual'].aggregate(rms).rename('LSTM_rmse')
@@ -25,10 +34,13 @@ def compute_metrics(predictions_filepath):
 
 def write_metrics(metrics_filepath, predictions_filepath):
     """
-    Write metrics to file
+    Write out model evaluation metrics to csv file.
+
+    :param metrics_filepath: Path to output csv for metrics
+    :param predictions_filepath: Path to csv with model predictions
+
     """
     metrics = compute_metrics(predictions_filepath)
-    # print(rms(preds['residual']), rmse_lake.median(), rmse_lake.mean())
     metrics.to_csv(metrics_filepath)
 
 
